dd6c54d5fc8c43462ebd8e5d19a8137213228349
masashi381 <121969638+masashi381@users.noreply.github.com>
2024-09-07 11:10:03 -0700

    add restaurants server

---
+++ rr-app/src/app/models/restaurantModels.ts
@@ -0,0 +1,35 @@
+import mongoose from "mongoose";
+
+const Schema = mongoose.Schema;
+const RestaurantSchema = new Schema({
+  restaurant_name: {
+    type: String,
+    required: true,
+  },
+  restaurant_avg_ratings: {
+    type: Number,
+    required: true,
+  },
+  restaurant_add: {
+    type: String,
+    required: true,
+  },
+  restaurant_tags: {
+    type: String, // Changed back to a single string
+    required: true,
+  },
+  restaurant_number_reviews: {
+    type: Number,
+    required: true,
+  },
+  reviewsId: [
+    {
+      type: String,
+      ref: "Review", // Reference to the Review model
+    },
+  ],
+});
+
+const RestaurantModel = mongoose.model("Restaurant", RestaurantSchema);
+
+export default RestaurantModel;
