f8d09bed4bd7a842cfae53c7b14f93d93aa4de8c
masashi381 <121969638+masashi381@users.noreply.github.com>
2024-09-11 08:04:00 -0700

    add user context

---
+++ rr-app/src/context/UserContext.tsx
@@ -0,0 +1,26 @@
+"use client";
+import { createContext, ReactNode, useState } from "react";
+import { UserInfo, LoginStatus, FirebaseAccount, UserContextProps } from "@/types/userTypes";
+
+export const UserContext = createContext<UserContextProps>({} as UserContextProps);
+
+export function UserContextProvider({ children }: { children: ReactNode }) {
+  const [user, setUser] = useState<UserInfo | null>(null);
+  const [firebaseAccount, setFirebaseAccount] = useState<FirebaseAccount | null>(null);
+  const [loginStatus, setLoginStatus] = useState(LoginStatus.Unknown);
+
+  return (
+    <UserContext.Provider
+      value={{
+        user,
+        setUser,
+        firebaseAccount,
+        setFirebaseAccount,
+        loginStatus,
+        setLoginStatus,
+      }}
+    >
+      {children}
+    </UserContext.Provider>
+  );
+}
