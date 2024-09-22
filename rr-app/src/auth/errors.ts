78d021926aed0ddc8d94e6e4a238ef19f04f2289
masashi381 <121969638+masashi381@users.noreply.github.com>
2024-09-22 10:17:09 -0700

    show error message

--- rr-app/src/auth/errors.ts
+++ rr-app/src/auth/errors.ts
@@ -1,11 +1,17 @@
 export const getErrorMessage = (errorCode: string): string => {
   switch (errorCode) {
-    case "auth/invalid-login-credentials":
+    case "auth/invalid-email":
+      return " Incorrect email or password";
+    case "auth/invalid-credential":
       return " Incorrect email or password";
     case "auth/email-already-in-use":
       return " This email is already registered";
     case "auth/weak-password":
       return "Password must be at least 6 characters long";
+    case "auth/missing-password":
+      return " Please enter a password";
+    case "auth/user-not-found":
+      return " User not found. Please sign up first.";
     default:
       return "Unexpected error. Please contact admin.";
   }
