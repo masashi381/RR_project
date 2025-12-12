0bb640f56425edfdabd03ec7dd4360cee83a4d17
masashi381 <121969638+masashi381@users.noreply.github.com>
2025-12-12 09:49:03 -0800

    　update rr-project part3

--- rr-app/eslint.config.mjs
+++ rr-app/eslint.config.mjs
@@ -1,8 +1,23 @@
-import next from "eslint-config-next";
+import nextPlugin from "eslint-plugin-next";
+import tsParser from "@typescript-eslint/parser";
+import tsPlugin from "@typescript-eslint/eslint-plugin";
 
 export default [
   {
-    ignores: ["node_modules", ".next"],
+    ignores: ["node_modules/**", ".next/**"],
   },
-  next,
+  {
+    plugins: {
+      "@typescript-eslint": tsPlugin,
+      next: nextPlugin,
+    },
+    languageOptions: {
+      parser: tsParser,
+    },
+    rules: {
+      // 必要なら追加
+      quotes: ["error", "double"],
+      indent: ["error", 2, { SwitchCase: 1 }],
+    },
+  },
 ];
