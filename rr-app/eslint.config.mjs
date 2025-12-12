8f899a5767b6a4a24815a056ecf42088a2f29586
masashi381 <121969638+masashi381@users.noreply.github.com>
2025-12-12 09:52:45 -0800

    　update rr-project part4

--- rr-app/eslint.config.mjs
+++ rr-app/eslint.config.mjs
@@ -1,4 +1,4 @@
-import nextPlugin from "eslint-plugin-next";
+import next from "eslint-config-next";
 import tsParser from "@typescript-eslint/parser";
 import tsPlugin from "@typescript-eslint/eslint-plugin";
 
@@ -6,16 +6,15 @@ export default [
   {
     ignores: ["node_modules/**", ".next/**"],
   },
+  next, // ← Next.js の推奨設定をそのまま追加
   {
     plugins: {
       "@typescript-eslint": tsPlugin,
-      next: nextPlugin,
     },
     languageOptions: {
       parser: tsParser,
     },
     rules: {
-      // 必要なら追加
       quotes: ["error", "double"],
       indent: ["error", 2, { SwitchCase: 1 }],
     },
